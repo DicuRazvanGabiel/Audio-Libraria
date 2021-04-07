203891e4334b251ae72d57a8072a950ca5b66d32
Razvan Dicu <dicu.razvan.gabriel@gmail.com>
2021-04-07 14:55:48 +0300

    working on player

--- src/screens/Player.js
+++ src/screens/Player.js
@@ -1,31 +1,35 @@
 import React, { useState, useEffect } from "react";
-import { View, Text } from "react-native";
+import { View, StyleSheet } from "react-native";
 import LoadingState from "../components/LoadingState";
 import firestore from "@react-native-firebase/firestore";
 import TrackPlayer from "react-native-track-player";
-import { Button } from "react-native-paper";
+import { Button, IconButton, Colors, Text } from "react-native-paper";
+
 const db = firestore();
 
 export default function Player({ route }) {
 	const [loading, setLoading] = useState(true);
+	const [playerState, setPlayerState] = useState("play");
 	const book = route.params.book;
 
 	const setUp = async () => {
 		const state = await TrackPlayer.getState();
 		if (state === TrackPlayer.STATE_PLAYING) {
+			TrackPlayer.stop();
 			TrackPlayer.destroy();
+			console.log("destroy");
 		}
-
+		const author = await db.collection("authors").doc(book.authors).get();
+		console.log(state);
 		await TrackPlayer.setupPlayer();
 
 		let trackArray = [];
-
 		book.chapters.map((c) => {
 			trackArray.push({
 				id: c.name,
 				url: c.file.src,
 				title: c.name,
-				artist: "deadmau5",
+				artist: author.data().name,
 				duration: c.duration,
 				artwork: book.image.src,
 			});
@@ -33,48 +37,94 @@ export default function Player({ route }) {
 		await TrackPlayer.add(trackArray);
 		const position = await TrackPlayer.getPosition();
 		const duration = await TrackPlayer.getDuration();
-		console.log(position);
-		console.log(duration);
+
+		TrackPlayer.play();
+		setPlayerState("play");
 		setLoading(false);
 	};
 
 	useEffect(() => {
 		setUp();
-	}, [book]);
+	}, [route, book]);
 
 	if (loading) {
 		return <LoadingState />;
 	}
 
+	const handlePlayPauseButton = () => {
+		if (playerState === "play") {
+			TrackPlayer.pause();
+			setPlayerState("pause");
+		}
+
+		if (playerState === "pause") {
+			TrackPlayer.play();
+			setPlayerState("play");
+		}
+	};
+
+	const handleSeek = async () => {};
+
 	return (
-		<View
-			style={{
-				flex: 1,
-				justifyContent: "space-evenly",
-				alignItems: "center",
-			}}
-		>
-			<Button
-				icon="play"
-				mode="contained"
-				onPress={() => TrackPlayer.play()}
-			>
-				Play
-			</Button>
-			<Button
-				icon="pause"
-				mode="contained"
-				onPress={() => TrackPlayer.pause()}
-			>
-				Pause
-			</Button>
-			<Button
-				icon="stop"
-				mode="contained"
-				onPress={() => TrackPlayer.stop()}
-			>
-				Stop
-			</Button>
+		<View style={styles.container}>
+			<Text>{book.title}</Text>
+
+			<View style={styles.mediaPleyerControls}>
+				<IconButton
+					icon="skip-previous-circle-outline"
+					color={Colors.red500}
+					size={playerIconSize}
+					onPress={() => TrackPlayer.skipToPrevious()}
+				/>
+
+				<IconButton
+					icon="rewind-10"
+					color={Colors.red500}
+					size={playerIconSize}
+					onPress={() => console.log("Pressed")}
+				/>
+
+				<IconButton
+					icon={
+						playerState === "play"
+							? "pause-circle-outline"
+							: "play-circle-outline"
+					}
+					color={Colors.red500}
+					size={playerIconSize + 15}
+					onPress={handlePlayPauseButton}
+				/>
+
+				<IconButton
+					icon="fast-forward-10"
+					color={Colors.red500}
+					size={playerIconSize}
+					onPress={() => console.log("Pressed")}
+				/>
+
+				<IconButton
+					icon="skip-next-circle-outline"
+					color={Colors.red500}
+					size={playerIconSize}
+					onPress={() => TrackPlayer.skipToNext()}
+				/>
+			</View>
 		</View>
 	);
 }
+
+const playerIconSize = 40;
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: "space-evenly",
+		alignItems: "center",
+	},
+	mediaPleyerControls: {
+		width: "100%",
+		flexDirection: "row",
+		justifyContent: "center",
+		alignItems: "center",
+	},
+});
