d6952d704b81f50804c663e7cbba9fa8bf950957
Razvan Dicu <dicu.razvan.gabriel@gmail.com>
2021-04-12 17:00:53 +0300

    icons for tab navigator

--- src/navigation/TabNavigator.js
+++ src/navigation/TabNavigator.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 import HomeNavigator from "./HomeNavigator";
 import SettingsScreen from "../screens/Settings";
@@ -8,9 +9,63 @@ export default function MainTabNavigator() {
 	const { Navigator, Screen } = createBottomTabNavigator();
 	return (
 		<Navigator>
-			<Screen name="Home" component={HomeNavigator} />
-			<Screen name="Explore" component={HomeNavigator} />
-			<Screen name="Settings" component={SettingsScreen} />
+			<Screen
+				name="Home"
+				component={HomeNavigator}
+				options={({ route }) => ({
+					tabBarIcon: ({ focused, color, size }) => {
+						let iconName = "home-outline";
+						if (focused) {
+							iconName = "home-sharp";
+						}
+						return (
+							<Ionicons
+								name={iconName}
+								size={size}
+								color={color}
+							/>
+						);
+					},
+				})}
+			/>
+			<Screen
+				name="My Books"
+				component={HomeNavigator}
+				options={({ route }) => ({
+					tabBarIcon: ({ focused, color, size }) => {
+						let iconName = "book-outline";
+						if (focused) {
+							iconName = "book-sharp";
+						}
+						return (
+							<Ionicons
+								name={iconName}
+								size={size}
+								color={color}
+							/>
+						);
+					},
+				})}
+			/>
+			<Screen
+				name="Wishlist"
+				component={SettingsScreen}
+				options={({ route }) => ({
+					tabBarIcon: ({ focused, color, size }) => {
+						let iconName = "star-outline";
+						if (focused) {
+							iconName = "star-sharp";
+						}
+						return (
+							<Ionicons
+								name={iconName}
+								size={size}
+								color={color}
+							/>
+						);
+					},
+				})}
+			/>
 		</Navigator>
 	);
 }
