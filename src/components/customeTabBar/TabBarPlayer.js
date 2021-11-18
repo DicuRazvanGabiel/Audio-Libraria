55ca6b1af19b2cb2c53dbdf363861c49228de263
Razvan Dicu <razvan.dicu@busymachine.com>
2021-11-18 20:21:15 +0200

    Add line to top of tab bars

--- src/components/customeTabBar/TabBarPlayer.js
+++ src/components/customeTabBar/TabBarPlayer.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { View } from "react-native";
-import { Text } from "react-native-paper";
+import { useTheme } from "react-native-paper";
 import { BottomTabBar } from "@react-navigation/bottom-tabs";
 import PlayerBar from "./PlayerBar";
 
 export default function TabBarPlayer(props) {
+	const theme = useTheme();
 	return (
-		<View>
+		<View style={{borderTopColor: theme.colors.primary , borderTopWidth: 1, paddingTop: 1}}>
 			<PlayerBar navigation={props.navigation} />
 			<BottomTabBar {...props} />
 		</View>
